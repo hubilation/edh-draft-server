2e2701c49bb02f4c9f58721e691547f3b066b37f
zack.huber <zack.huber@gmail.com>
2017-08-22 23:52:41 -0700

    blah

---
+++ src/authentication.js
@@ -0,0 +1,9 @@
+// import passport from 'passport';
+// import {LocalStrategy} from 'passport-local';
+
+// passport.use(new LocalStrategy(
+//     function(username, password, done){
+//         return done(null, {email:"test", name:"steve"});
+//     }
+// ))
+
